ebad37edc5de04ad774b746dedff5b1d119c03ef
miquintero <schaeffer.quintero@gmail.com>
2019-10-27 22:00:24 -0300

    feat: added route to request in controller

--- server/router.js
+++ server/router.js
@@ -3,4 +3,8 @@
 const express =  require('express');
 const router = express.Router();
 
+const controller = require('./controller/articles');
+
+router.get('/articles', controller.getArticles);
+
 module.exports = router;
